772d0d52184afe1af7f7f85b41c5c2fa91f697cd
boogie <1269594354@qq.com>
2022-02-19 20:19:01 +0800

    xinye: add changePrice

--- day1/react-demo/src/App.tsx
+++ day1/react-demo/src/App.tsx
@@ -1,8 +1,10 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {Container} from "react-bootstrap";
+import { Navba } from "./components/Navba.tsx";
 import Product from './components/Product.tsx';
 
 const App = () => {
-    let [products, setProducts] = React.useState([])
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         fetch("/products")
@@ -11,11 +13,14 @@ const App = () => {
     }, [])
 
   return (
-    <div style={{display:'flex',flexWrap: 'wrap'}}>
-        {products.map((product,index) => (
-            <Product key={index}  product={product}/>
-        ))}
-    </div>
+      <Container>
+          <Navba/>
+          <div style={{display:'flex',flexWrap: 'wrap'}}>
+              {products.map((product,index) => (
+                  <Product key={index} product={product}/>
+              ))}
+          </div>
+      </Container>
   );
 }
 
--- day1/react-demo/src/components/Product.tsx
+++ day1/react-demo/src/components/Product.tsx
@@ -1,35 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
+import {Button, styled, TextField} from "@mui/material";
 
 interface ComponentProps<T> {
     product:ProductData
 }
-export interface ProductData{
+
+interface ProductData{
     img:string;
     shortDescription:string;
-    price:double;
+    price:string;
     link:string
 }
-const Product= function(props:ComponentProps<ProductData>){
-    const navigate = useNavigate();
 
-    const clickProduct = function(link){
+const Product = (props:ComponentProps<ProductData>)=>{
+    const navigate = useNavigate();
+    const {product} = props;
+    const [isEditing,setIsEditing]=useState(false);
+
+
+    const clickProductImg = function(link){
         navigate(link);
     }
 
+    const changePrice=function () {
+        setIsEditing(true);
+    }
+
+    const savePrice=()=>{
+    }
+
+
+    const inputTextChange=function (value:string) {
+        editingProduct.price=value;
+    }
+
+    const PriceInput=styled('div')({
+        width:'150px',
+        input:{
+            height:'9px',
+            textAlign:'center'
+        }
+    })
+
     return (
-       <Card style={{ width: '12rem',margin: '8px',borderRadius: '16px' }} onClick={() => clickProduct(props.product.link)}>
-       <Card.Img variant="top" src={props.product.img} />
+       <Card style={{ width: '12rem',margin: '8px',borderRadius: '16px' }} >
+       <Card.Img variant="top" src={product.img} onClick={() => clickProductImg(product.link)}/>
        <Card.Body>
          <Card.Text>
-           {props.product.shortDescription}
+           {product.shortDescription}
          </Card.Text>
-         <font color="error">¥ {props.product.price}</font>
+           <div>
+           {
+               isEditing?
+                   <PriceInput>
+                       <TextField size={"small"} variant={"outlined"} defaultValue={product.price} onChange={(v)=>{inputTextChange(v.target.value)}}/>
+                       <Button onClick={()=>savePrice()}>Save</Button>
+                   </PriceInput>
+                       :<font color="error">¥ {product.price}</font>
+           }
+           <button  onClick={()=>changePrice()}>change Price</button>
+           </div>
        </Card.Body>
        </Card>
     );
-
 };
 
 export default Product;
