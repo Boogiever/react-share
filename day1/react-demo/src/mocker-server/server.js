388e9993a629a445762c1e6dd8d94bb88d4f5107
boogie <1269594354@qq.com>
2022-02-20 22:46:13 +0800

    [mirage] use timing to simulate a slow server & return a custom response

--- day1/react-demo/src/mocker-server/server.js
+++ day1/react-demo/src/mocker-server/server.js
@@ -1,4 +1,4 @@
-import {createServer} from 'miragejs'
+import {createServer, Response} from 'miragejs'
 
 export default function makeServer() {
   createServer({
@@ -9,7 +9,16 @@ export default function makeServer() {
           { id:"1",img: "/resources/product2.png", shortDescription: "男鞋冬款加绒保暖防臭男士网面运动2021春秋休闲皮鞋内增", price: "98",link:"/product/2" },
           { id:"2",img: "/resources/product3.png", shortDescription: "老虎玩偶虎年吉祥物公仔毛绒玩具仿真白虎娃娃小号东北虎", price: "19.8",link:"/product/3" },
         ],
-      }))
+      }),
+      { timing: 4000 })
+
+      // Using the `Response` class to return a 500
+      this.delete("/movies/1", () => {
+        let headers = {}
+        let data = { errors: ["Server did not respond"] }
+
+        return new Response(500, headers, data);
+      })
     },
   })
 }
