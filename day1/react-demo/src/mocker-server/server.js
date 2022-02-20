342e885c4fd5745d1214a44c19da4417faaba4bf
boogie <1269594354@qq.com>
2022-02-20 22:48:19 +0800

    [mirage] use Model and seeds to store data in in-memory database

--- day1/react-demo/src/mocker-server/server.js
+++ day1/react-demo/src/mocker-server/server.js
@@ -1,24 +1,28 @@
-import {createServer, Response} from 'miragejs'
+import {createServer, Model, Response} from 'miragejs'
 
 export default function makeServer() {
   createServer({
+
+    models: {
+      product: Model,
+    },
+
     routes() {
-      this.get("/products", () => ({
-        products: [
-          { id:"0",img: "/resources/product1.png", shortDescription: "不要后背豆豆 背部豆豆说拜拜 背部无豆清爽一夏 男女通用买", price: "48",link:"/product/1" },
-          { id:"1",img: "/resources/product2.png", shortDescription: "男鞋冬款加绒保暖防臭男士网面运动2021春秋休闲皮鞋内增", price: "98",link:"/product/2" },
-          { id:"2",img: "/resources/product3.png", shortDescription: "老虎玩偶虎年吉祥物公仔毛绒玩具仿真白虎娃娃小号东北虎", price: "19.8",link:"/product/3" },
-        ],
-      }),
-      { timing: 4000 })
-
-      // Using the `Response` class to return a 500
-      this.delete("/movies/1", () => {
-        let headers = {}
-        let data = { errors: ["Server did not respond"] }
-
-        return new Response(500, headers, data);
+      this.get("/products", (schema, request) => {
+        return schema.products.all()
       })
+
+      this.get("/products/:id", (schema, request) => {
+        let id = request.params.id
+
+        return schema.products.find(id)
+      })
+    },
+
+    seeds(server) {
+      server.create("product", { img: "/resources/product1.png", shortDescription: "不要后背豆豆 背部豆豆说拜拜 背部无豆清爽一夏 男女通用买", price: "48",link:"/product/1" })
+      server.create("product", { img: "/resources/product2.png", shortDescription: "男鞋冬款加绒保暖防臭男士网面运动2021春秋休闲皮鞋内增", price: "98",link:"/product/2" })
+      server.create("product", { img: "/resources/product3.png", shortDescription: "老虎玩偶虎年吉祥物公仔毛绒玩具仿真白虎娃娃小号东北虎", price: "19.8",link:"/product/3" })
     },
   })
 }
